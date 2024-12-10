1e9dd478353b054efb08363a75cb133c557d6204
rUv <ruv@ruv.net>
2024-12-10 21:21:37 +0000

    feat: Add new blockchain entries and enhance start script with color coding

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Function to print colored header
@@ -65,7 +66,7 @@ start_docker() {
                 ;;
             6)
                 echo -e "\n${BLUE}Container Health Status:${NC}"
-                docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Health}}"
+                docker ps --format "table {{.Names}}\t{{.Status}}"
                 ;;
             7)
                 return
